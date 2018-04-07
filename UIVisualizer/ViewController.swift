5356428250d3230208b50526c17f2135337d61d8
Michael Edenzon <edenzonmp@g.cofc.edu>
2018-04-07 13:42:23 -0400

    Initial Commit

---
+++ UIVisualizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIVisualizer
+//
+//  Created by Michael Edenzon on 4/7/18.
+//  Copyright © 2018 Michael Edenzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
